d7bb720993515ae1fa8f3e4b225dedce65100500
Username <albavmolinanyc@gmail.com>
2022-07-14 15:33:59 -0400

    add urls and views for memo app

--- system/memos/urls.py
+++ system/memos/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import *
+
 
 urlpatterns = [
-  
+    path('', views.memo, name='memo'),
+    path('add_memo/<int:job_id>/', views.add_memo, name='add_memo'),
+    path('remove_memo/<int:job_id>/<int:memo_item_id>/', views.remove_memo, name='remove_memo'),
+    path('remove_memo_item/<int:job_id>/<int:memo_item_id>/', views.remove_memo_item, name='remove_memo_item'),
 
-   
-  
-
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- system/memos/views.py
+++ system/memos/views.py
@@ -1,4 +1,66 @@
-from django.shortcuts import render
+#memos aka carts
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Job, MemoItem, Memo
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
-#memos aka carts
+from django.http import HttpResponse
+
+# create private function from this request we are going to take the memo.id
+def _memo_id(request):
+    # session_key -> session_id,
+    memo = request.session.session_key
+    if not memo:
+        memo = request.session.create()
+    # return the memo & memo id
+    return memo
+
+# decrement and remove memoItem aka job by 1
+def remove_memo_item(request, job_id):
+    memo = Memo.objects.get(memo_id=_memo_id(request))
+    job = get_object_or_404(Job, id=job_id)
+    memo_item = MemoItem.objects.get(job=job, memo=memo)
+    memo_item.delete()
+    return redirect('memo')
+
+# add job inside the memo, so need job_id
+def add_memo(request, job_id):
+    job = Job.objects.get(id=job_id) #get the job
+    try: 
+        memo = Memo.objects.get(memo_id=_memo_id(request)) # the memo using the memo_id present in the session
+    except Memo.DoesNotExist:
+        memo = Memo.objects.create(
+            memo_id = _memo_id(request)
+        )
+    memo.save() # up to this point we have a job and the cart put the job inside the cart, the job becomes cartItem
+
+    try:
+        memo_item = MemoItem.objects.get(job=job, memo=memo)
+        memo_item.quantity += 1 # want to increment memo by 1
+        memo_item.save()
+    except MemoItem.DoesNotExist:
+        memo_item = MemoItem.objects.create(
+            job = job,# pass in which job should be in the memo
+            quantity = 1,
+            memo = memo, # we now create the cart 
+        )
+        memo_item.save() # save the memoitem
+   
+    return redirect('memo') # redirect the user to the memo page
+
+def memo(request, quantity=0, memo_items=None): 
+    try: 
+        memo = Memo.objects.get(memo_id=_memo_id(request))
+        memo_items = MemoItem.objects.filter(memo=memo, is_active=True)
+
+        for memo_item in memo_items:
+            quantity += memo_item.quantity
+    except ObjectDoesNotExist: # but if the memo_item does not exst pass
+        pass
+
+    context = {
+        'quantity': quantity,
+        'memo_items': memo_items,
+    }
+    return render(request, 'memo.html', context)
