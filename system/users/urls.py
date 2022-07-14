046027875ad2e768b0d0bd3ac4b33c9b58f07313
Username <albavmolinanyc@gmail.com>
2022-07-14 15:22:52 -0400

    does users app

--- system/users/urls.py
+++ system/users/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from . import views
-from .views import *
+from .views import UserRegisterView
 
 urlpatterns = [
-  
-
-   
+    path('register/', UserRegisterView.as_view(), name='register'),
   
 
 ]
